8015ed687dc7e926a8da073d1d70c271fdcf38e2
chinni524 <chinni524@gmail.com>
2019-05-25 09:06:17 +0000

    samplejava

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+            for(int i=1;i<=10;i++)
+            {
+            System.out.println("Hello, World"+i);
+            }
+    }
+}
